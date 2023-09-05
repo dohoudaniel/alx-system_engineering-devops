6a969fb8df5329c7cbba3bd9f71ab72ab5cae9c5
dohoudaniel <dohoudanielfavour@gmail.com>
2023-09-05 12:26:42 +0100

    A manifest file that kills a process named 'killmenow'.

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,9 @@
+# A Puppet manifest file that kills a process named killmenow
+# This manifest file:
+#    Uses the 'exec' Puppet resource 
+#    Uses 'pkill'
+exec { 'kill':
+    command => ''pkill -f killmenow,
+    onlyif  => 'pgrep -f killmenow',
+    path    => ['/bin', '/usr/bin'],
+}
