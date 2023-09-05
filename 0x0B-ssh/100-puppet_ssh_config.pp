0b084c131b6eacc4d90b24d8c4427445eacbad45
dohoudaniel <dohoudanielfavour@gmail.com>
2023-09-05 22:55:54 +0100

    Edit Puppet script and remove '/root/'.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # A Puppet manifest file that sets up your client SSH configuration file
 # so that you can connect to a server without typing a password.
-file_line { 'Turn off password authentication':
+file_line { 'Turn off password auth':
     ensure => 'present',
     path   => '/etc/ssh/ssh_config',
     line   => '    PasswordAuthentication no',
 }
 
-file_line { 'Declaringt the identity file':
+file_line { 'Declaring identity file':
     ensure => 'present',
     path   => '/etc/ssh/ssh_config',
-    line   => '    IdentityFile /root/.ssh/school',
+    line   => '    IdentityFile ~/.ssh/school',
 }
