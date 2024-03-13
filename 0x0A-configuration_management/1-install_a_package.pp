7fa8a4196ac0e7edc767fbbdb198522508849e0a
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-13 07:55:23 +0100

    Update Puppet script

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,12 @@
 # A Puppet file that installs flask from pip3
-exec {'apt-get update':
-    command => '/usr/bin/apt-get update',
-}
-exec {'pip3 install Flask==2.1.0':
-    ensure  => 'installed',
-    require => Exec['apt-get update'],
+# exec {'apt-get update':
+#    command => '/usr/bin/apt-get update',
+# }
+# exec {'pip3 install Flask==2.1.0':
+#     ensure  => 'installed',
+#     require => Exec['apt-get update'],
+# }
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
