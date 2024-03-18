8936d863a75822d4084341ff218eaa086ce3345a
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-18 19:52:02 +0100

    Edit redirect statement to valid URL

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,7 +16,7 @@ exec {'Hello':
 }
 
 exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation
-\/redirect_me {\\n\\t\\treturn 301 http:\/\/learnopolia.tech;\\n\\t}/" /etc/nginx/sites-available/default':
+\/redirect_me {\\n\\t\\treturn 301 https:\/\/x.com;\\n\\t}/" /etc/nginx/sites-available/default':
     provider => shell,
 }
 
