828a2e56d3c405fd7096667ebce6b3bcd0a005b1
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-18 19:20:38 +0100

    A Puppet manifest that configures a web server according to some requirements

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+# A Puppet manifest that configures a server. It:
+# - Updates the server
+# - Installs an Nginx server
+# - Configures the Nginx web server to
+#	- return a page that contains the string "Hello World!" when queried at its root / with a GET request (requesting a page) using curl
+#	- A redirection must be a “301 Moved Permanently”
+#
+exec {'install':
+    command  => 'sudo apt update ; sudo apt-get -y install nginx',
+    provider => shell,
+}
+
+exec {'Hello':
+    command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+    provider => shell,
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation
+\/redirect_me {\\n\\t\\treturn 301 https:\/\/x.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+    provider => shell,
+}
+
+exec {'run':
+    command  => 'sudo service nginx restart',
+    provider => shell,
+}
