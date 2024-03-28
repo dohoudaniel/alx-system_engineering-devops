d89dbd362e57ea362e216441f28d78b96131de9e
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-28 23:27:38 +0100

    Update failing Puppet script

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,43 +5,20 @@
 #	- return a page that contains the string "Hello World!" when queried at its root / with a GET request (requesting a page) using curl
 #	- A redirection must be a “301 Moved Permanently”
 #
-# Updating the system
-exec { 'update system':
-  command  => '/usr/bin/apt-get update',
+exec {'update':
+  command => 'sudo apt-get update ; sudo apt-get -y upgrade',
 }
 
-# Installing Nginx package
-package { 'nginx':
-  ensure   => installed,
+package {'nginx':
+  ensure => 'present',
 }
 
-# Create an index.html file
-file { '/var/www/html/index.html':
-  content  => 'Hello World!',
+file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
 
-# Add a redirection rule in Nginx configuration
-exec { 'redirect_me':
-  command  => 'sed -i "10i\	return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell',
-}
-
-# Add a custom HTTP header in Nginx configuration
-exec { 'HTTP header':
-  command  => 'sed -i "8i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-  provider => 'shell',
-}
-
-# Create a separate custom header file
-file { '/etc/nginx/conf.d/custom_headers.conf':
-  ensure   => present,
-  content  => "add_header X-Served-By $hostname;",
-  notify   => Service['nginx'],
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure   => running,
-  enable   => true,
-  require  => [Package['nginx'], File['/etc/nginx/conf.d/custom_headers.conf']],
+exec {'run':
+  command => 'sudo service nginx restart',
 }
