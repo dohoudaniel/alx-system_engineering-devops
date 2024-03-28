c0d07a487b099d295584bcb149801a89fd20c5a7
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-29 00:07:21 +0100

    Reinstall Puppet on my Web-02 server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,50 +1,15 @@
-# A Puppet manifest that configures a server. It:
-# - Updates the server
-# - Installs an Nginx server
-# - Configures the Nginx web server to
-#	- return a page that contains the string "Hello World!" when queried at its root / with a GET request (requesting a page) using curl
-#	- A redirection must be a “301 Moved Permanently”
-#
-# Ensure Nginx is installed and running
-package { 'nginx':
-  ensure => installed,
+#A Bash Script that runs task 0 but as a Puppet file
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => Package['nginx'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-# Create index.html file
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello, World!',
-  require => Service['nginx'],
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-# Define custom HTTP response header
-$hostname = $facts['networking']['hostname']
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-        index index.html;
-
-        add_header X-Served-By ${hostname};
-        location /redirect_me {
-            return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-        location / {
-            try_files \$uri \$uri/ =404;
-        }
-    }
-  ",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
