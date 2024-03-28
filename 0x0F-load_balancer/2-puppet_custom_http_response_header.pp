7cf23680cf0f835fe3f92adbcb7555fb8149aed5
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-28 23:05:52 +0100

    Update Puppet script

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,38 +5,43 @@
 #	- return a page that contains the string "Hello World!" when queried at its root / with a GET request (requesting a page) using curl
 #	- A redirection must be a “301 Moved Permanently”
 #
-package {'nginx':
-    ensure   => 'present',
+# Updating the system
+exec { 'update system':
+  command  => '/usr/bin/apt-get update',
 }
 
-exec {'install':
-    command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
-    provider => shell,
+# Installing Nginx package
+package { 'nginx':
+  ensure   => installed,
 }
 
-exec {'Hello':
-    command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-    provider => shell,
+# Create an index.html file
+file { '/var/www/html/index.html':
+  content  => 'Hello World!',
 }
 
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com/watch?v=QH2-TGUlwu4;\\n\\t}/" /etc/nginx/sites-available/default':
-    provider => shell,
+# Add a redirection rule in Nginx configuration
+exec { 'redirect_me':
+  command  => 'sed -i "10i\	return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
 }
 
-# Adding a custom HTTP header in Nginx configuration
+# Add a custom HTTP header in Nginx configuration
 exec { 'HTTP header':
-    command  => 'sed -i "8i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-    provider => 'shell',
+  command  => 'sed -i "8i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  provider => 'shell',
 }
 
 # Create a separate custom header file
 file { '/etc/nginx/conf.d/custom_headers.conf':
-    ensure   => present,
-    content  => "add_header X-Served-By $hostname;",
-    notify   => Service['nginx'],
+  ensure   => present,
+  content  => "add_header X-Served-By $hostname;",
+  notify   => Service['nginx'],
 }
 
-exec {'run':
-    command  => 'sudo service nginx restart',
-    provider => shell,
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure   => running,
+  enable   => true,
+  require  => [Package['nginx'], File['/etc/nginx/conf.d/custom_headers.conf']],
 }
