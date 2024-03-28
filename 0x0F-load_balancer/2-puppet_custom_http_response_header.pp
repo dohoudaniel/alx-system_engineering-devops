0de9165e52e75cf4944751aba1ec8c2d20c9eaa2
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-28 23:55:13 +0100

    Fix file with AI

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,20 +5,46 @@
 #	- return a page that contains the string "Hello World!" when queried at its root / with a GET request (requesting a page) using curl
 #	- A redirection must be a “301 Moved Permanently”
 #
-exec {'update':
-  command => 'sudo apt-get update ; sudo apt-get -y upgrade',
+# Ensure Nginx is installed and running
+package { 'nginx':
+  ensure => installed,
 }
 
-package {'nginx':
-  ensure => 'present',
+service { 'nginx':
+  ensure => running,
+  enable => true,
+  require => Package['nginx'],
 }
 
-file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+# Create index.html file
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello, World!',
+  require => Service['nginx'],
 }
 
-exec {'run':
-  command => 'sudo service nginx restart',
+# Define custom HTTP response header
+$hostname = $facts['networking']['hostname']
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+    server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+        index index.html;
+
+        add_header X-Served-By ${hostname};
+        location /redirect_me {
+            return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+        location / {
+            try_files \$uri \$uri/ =404;
+        }
+    }
+  ",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
