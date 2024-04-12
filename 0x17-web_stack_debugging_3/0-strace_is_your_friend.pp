6c7c55e646a724168d5b2bcab74854fa9139d080
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-12 17:36:49 +0100

    Puppet lint file: Remove uppercase letters

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # A puppet manuscript to replace a line in a file on a server
-$fileToEdit = '/var/www/html/wp-settings.php'
+$filetoedit = '/var/www/html/wp-settings.php'
 # Replace the line in the wp-settings.php file containing "phpp" with "php"
 exec { 'fix-wordpress':
-  command => "sed -i 's/phpp/php/g' ${fileToEdit}",
+  command => "sed -i 's/phpp/php/g' ${filetoedit}",
   path    => ['/bin','/usr/bin']
 }
