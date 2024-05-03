8f0630f85315a470052627532c64800517cbeab7
dohoudaniel <dohoudanielfavour@gmail.com>
2024-05-03 20:40:19 +0100

    Remove user_agent statement

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -23,8 +23,8 @@ def number_of_subscribers(subreddit):
         return 0
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    user_agent = {"User-Agent": "Mozilla/5.0"}
-    response = get(url, headers=user_agent)
+    # user_agent = {"User-Agent": "Mozilla/5.0"}
+    response = get(url)
     results = response.json()
 
     try:
