634d2a744f78654657e365ac58cf569bc63714eb
dohoudaniel <dohoudanielfavour@gmail.com>
2024-05-03 15:37:59 +0100

    A Python script that exports user data into a CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A Python script that uses a REST API, for a
+given employee ID to return information about
+his/her TODO list progress.
+This script goes further to record all tasks that
+are owned by the employee into a CSV file of the format:
+UserId, Username, TaskCompletedStatus, TaskTitle
+The CSV file name is of the format: UserId.csv
+"""
+
+import requests
+from sys import argv as cmdArg
+
+
+if __name__ == "__main__":
+    employeeId = cmdArg[1]
+    usersUrl = "https://jsonplaceholder.typicode.com/users"
+    employeeUrl = usersUrl + "/" + employeeId
+
+    response = requests.get(employeeUrl)
+    finalResponse = response.json()
+    username = finalResponse.get('username')
+    taskStatus = finalResponse.get('completed')
+    taskTitle = finalResponse.get('title')
+
+    # Storing the user data into a UserId.csv file
+    with open('{}.csv'.format(employeeId), 'w') as myCSVFileObject:
+        for task in tasks:
+            myCSVFileObject.write('"{}", "{}", "{}", "{}"'.format(
+                employeeId, username, taskStatus, taskTitle))
