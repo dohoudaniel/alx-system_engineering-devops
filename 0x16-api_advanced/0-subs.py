a25a07bc818088820d3fefd960df7048a79af233
dohoudaniel <dohoudanielfavour@gmail.com>
2024-05-03 21:02:44 +0100

    Comment out redirects and change use of headers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -22,10 +22,12 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    # user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent, allow_redirects=False)
+    response = get(url) #, headers=user_agent, allow_redirects=False)
     results = response.json()
 
     if response.status_code == 200:
         return results.get('data').get('subscribers')
+    else:
+        return 0
