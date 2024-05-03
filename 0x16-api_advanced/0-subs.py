fd9128d4391c60d0d61be5ded447b704977c5635
dohoudaniel <dohoudanielfavour@gmail.com>
2024-05-03 21:04:27 +0100

    Add redirects and change use of headers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -22,9 +22,9 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    # user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    headers = {'User-Agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url) #, headers=user_agent, allow_redirects=False)
+    response = get(url, headers=user_agent, allow_redirects=False)
     results = response.json()
 
     if response.status_code == 200:
