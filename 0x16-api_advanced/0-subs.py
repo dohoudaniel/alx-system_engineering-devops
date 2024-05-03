cd2ffb36c45820cadba5f1ee945e2cfde71b5998
dohoudaniel <dohoudanielfavour@gmail.com>
2024-05-03 20:34:52 +0100

    Add module documentation and correct typo

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,17 +12,22 @@ from requests import get
 
 def number_of_subscribers(subreddit):
     """
-    Returns the number of subscribers for a given subreddit
+    A function that returns the number of subscribers
+    for a given subreddit.
+    Arguments:
+        subreddit (str): The subreddit to query.
+    Returns:
+        The number of subscribers, or 0 if the subreddit is invalid.
     """
-    if subreddit is None or not isinstance(subreddit, st)r:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    user_agent = {'User-Agent': 'Mozilla/5.0'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    user_agent = {"User-Agent": "Mozilla/5.0"}
     response = get(url, headers=user_agent, allow_redirects=False)
 
     try:
-        return response.get('data').get('subscribers')
+        return response.get("data").get("subscribers")
 
     except Exception:
         return 0
