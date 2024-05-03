d42830ba67649cf0d97b8ff1eac0a967c3edd713
dohoudaniel <dohoudanielfavour@gmail.com>
2024-05-03 20:37:02 +0100

    Remove redirect statements

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -24,7 +24,7 @@ def number_of_subscribers(subreddit):
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     user_agent = {"User-Agent": "Mozilla/5.0"}
-    response = get(url, headers=user_agent, allow_redirects=False)
+    response = get(url, headers=user_agent)
 
     try:
         return response.get("data").get("subscribers")
