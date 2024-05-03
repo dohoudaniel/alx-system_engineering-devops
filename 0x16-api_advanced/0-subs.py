e1a1e7181f4ef59bc088e1c9696712b2bf2eed1e
dohoudaniel <dohoudanielfavour@gmail.com>
2024-05-03 20:51:09 +0100

    swap variable declaration

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -22,8 +22,8 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
 
